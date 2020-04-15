930f6502fa78e035a74327357c202e6ef5ef62c2
robin-lillqvist <robin.lillqvist@gmail.com>
2020-04-15 14:18:07 +0200

    trying to fix semaphore passing tests

--- cypress/integration/userCanAddProductsToRequest.feature.js
+++ cypress/integration/userCanAddProductsToRequest.feature.js
@@ -1,6 +1,6 @@
 describe("when products are visible", () => {
   before(() => {
-    //   cy.exec("yarn start")
+      cy.exec("yarn start")
     cy.server();
     cy.route({
       method: "GET",
--- cypress/integration/userCanSeeTitle.feature.js
+++ cypress/integration/userCanSeeTitle.feature.js
@@ -1,13 +1,14 @@
 describe("Free Article view:", () => {
-  beforeEach(() => {
+  before(() => {
+    cy.exec("yarn start");
     cy.visit("/");
   });
 
-  it('User can see main page', () => {
-    cy.get('.title').should('contain', 'Helping hand') 
+  it("User can see main page", () => {
+    cy.get(".title").should("contain", "Helping hand");
   });
 
-  it('User can navigate to create a task', () => {
-    cy.get('button').contains('Create your request')
+  it("User can navigate to create a task", () => {
+    cy.get("button").contains("Create your request");
   });
-})
+});
