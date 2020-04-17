80a267063d2b9b761c408a05cbd98a591e2b21ac
Blake <blake@futchi.com>
2020-04-17 14:07:19 +0200

    centers help button

--- src/components/ClaimTask.jsx
+++ src/components/ClaimTask.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect, useDispatch } from "react-redux";
-import { Button, List, Container, Grid } from "semantic-ui-react";
+import { Button, Grid } from "semantic-ui-react";
 import DisplayMap from "./DisplayMap";
 
 const ClaimTask = (props) => {
@@ -14,9 +14,11 @@ const ClaimTask = (props) => {
   }
   if (props.userID) {
     claimButton = (
-      <Button id="create-request" onClick={dispatch({ type: "SHOW_MAP" })}>
-        Offer help
-      </Button>
+      <Grid.Column align='center'>
+        <Button id="create-request" onClick={dispatch({ type: "SHOW_MAP" })}>
+          Offer help
+        </Button>
+      </Grid.Column>
     );
   }
 
