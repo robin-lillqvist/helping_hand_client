3945de0a55461bbce3aba41bdcee2abb2b243db8
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-04-17 10:33:08 +0200

    Updates Map component with markers - WIP Co-authored-by: robin-lillqvist Robin.lillqvist@gmail.com> Co-authored-by: blake-futchi <blake@futchi.com>

--- src/components/DisplayMap.jsx
+++ src/components/DisplayMap.jsx
@@ -2,55 +2,76 @@ import React, { Component } from "react";
 import { Marker, Map, InfoWindow, GoogleApiWrapper } from "google-maps-react";
 
 
-const style = {
-  width: "75%",
-  height: "75%",
-  borderRadius: "10px",
-  left: "12%",
-};
-
-let markers = 
-  [{lat: 57.7089, lng: 11.9746 }]
-
 class MapContainer extends Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
-    markers: {}
-  };
-
-  render() {
+    selectedPlace: {}
+  }
+  render () {
+    let locations = {
+      cities: [
+        {
+          id: 1,
+          name: 'Stockholm',
+          position: { lat: 59.3285865, lng: 18.0599052 }
+        },
+        {
+          id: 2,
+          name: 'Tellusborgsvägen',
+          position: { lat: 59.3042815, lng: 18.0105969 }
+        },
+        {
+          id: 3,
+          name: 'Gothenburg',
+          position: { lat: 57.7089, lng: 11.9746 }
+        }
+      ]
+    }
+    const style = {
+      width: '100%',
+      height: '100%'
+    }
+    const onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      })
+    // const onMapClicked = props => {
+    //   if (this.state.showingInfoWindow) {
+    //     this.setState({
+    //       showingInfoWindow: false,
+    //       activeMarker: null
+    //     })
+    //   }
+    // }
     return (
-
-
       <Map
         google={this.props.google}
-        zoom={3}
         style={style}
         initialCenter={{
-          lat: 57.7089,
-          lng: 11.9746,
+          lat: 59.3293,
+          lng: 18.0686
         }}
+        zoom={15}
+        onClick={this.onMapClicked}
       >
-
-        {markers.map((city) => {
-          return (
-            <>
-              <Marker
-                lat= {city.lat}
-                lng= {city.lng}
-                name= {city.name}
-                color="blue"
-              />
-            </>
-          );
-        })}
-
-        <InfoWindow>
-          <div></div>
+        {locations.cities.map(city => (
+          <Marker
+            title={city.name}
+            name={city.name}
+            key={city.id}
+            position={city.position}
+            onClick={onMarkerClick.bind(this)}
+          />
+        ))}
+        {/* <Marker onClick={this.onMarkerClick} name={'Current location'} /> */}
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>{/* <h1>{this.state.selectedPlace.name}</h1> */}</div>
         </InfoWindow>
       </Map>
-    );
+    )
   }
 }
 
