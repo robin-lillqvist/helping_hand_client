5f397e6cc5cc755cc0e46f16b4a66273425a28ab
robin-lillqvist <robin.lillqvist@gmail.com>
2020-04-20 11:58:49 +0200

    testkjhfkjf

--- src/modules/authentication.js
+++ src/modules/authentication.js
@@ -41,6 +41,9 @@ const onRegister = (event, dispatch) => {
       password_confirmation: event.target.elements.password_confirmation.value,
     })
     .then((response) => {
+      localStorage.setItem("J-tockAuth-Storage",
+      JSON.stringify(response.headers)
+      )
       dispatch({
         type: "AUTHENTICATE",
         payload: {
