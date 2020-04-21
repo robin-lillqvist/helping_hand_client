cbb2ed9d3404db9715820b285886f5246b7d2fc2
robin-lillqvist <robin.lillqvist@gmail.com>
2020-04-21 14:39:36 +0200

    forgot to uncomment tests

--- cypress/integration/userCanCreateTask.feature.js
+++ cypress/integration/userCanCreateTask.feature.js
@@ -14,71 +14,71 @@ describe("User can submit destination", () => {
     cy.login();
   });
 
-  // it("Successfully", () => {
-  //   cy.route({
-  //     method: "GET",
-  //     url: "https://maps.googleapis.com/maps/api/geocode/json?**",
-  //     response: "fixture:address_to_coords_response.json",
-  //     params: {
-  //       address: "Rome",
-  //       key: process.env.REACT_APP_GOOGLE_APIKEY,
-  //     },
-  //   });
-  //   cy.get("#create-request").click();
-  //   cy.get("#product-1").should("not.exist");
-  //   cy.get("#addressInput").type("Rome");
-  //   cy.get("#addressConfirm").click();
-  //   cy.route({
-  //     method: "PUT",
-  //     url: "**/tasks/1",
-  //     response: "fixture:task_list_updated_total.json",
-  //   });
-  //   cy.get("#product-1").within(() => {
-  //     cy.get("button").should("exist");
-  //     cy.get("button").click();
-  //   });
-  //   cy.get("#request-list").within(() => {
-  //     cy.get("#Potatoes").should("contain", "1 x Potatoes");
-  //     cy.get("#orderTotal").should("contain", "98.0");
-  //   });
-  //   cy.get("#product-6").within(() => {
-  //     cy.get("button").click();
-  //     cy.get("button").click();
-  //     cy.get("button").click();
-  //     cy.get("button").click();
-  //   });
-  //   cy.route({
-  //     method: "PUT",
-  //     url: "**/tasks/1",
-  //     response: "fixture:task_confirmation_response.json",
-  //   });
-  //   cy.get("#confirm-task").click();
-  //   cy.get("#success-message").should(
-  //     "contain",
-  //     "Your task has been confirmed"
-  //   );
-  // });
+  it("Successfully", () => {
+    cy.route({
+      method: "GET",
+      url: "https://maps.googleapis.com/maps/api/geocode/json?**",
+      response: "fixture:address_to_coords_response.json",
+      params: {
+        address: "Rome",
+        key: process.env.REACT_APP_GOOGLE_APIKEY,
+      },
+    });
+    cy.get("#create-request").click();
+    cy.get("#product-1").should("not.exist");
+    cy.get("#addressInput").type("Rome");
+    cy.get("#addressConfirm").click();
+    cy.route({
+      method: "PUT",
+      url: "**/tasks/1",
+      response: "fixture:task_list_updated_total.json",
+    });
+    cy.get("#product-1").within(() => {
+      cy.get("button").should("exist");
+      cy.get("button").click();
+    });
+    cy.get("#request-list").within(() => {
+      cy.get("#Potatoes").should("contain", "1 x Potatoes");
+      cy.get("#orderTotal").should("contain", "98.0");
+    });
+    cy.get("#product-6").within(() => {
+      cy.get("button").click();
+      cy.get("button").click();
+      cy.get("button").click();
+      cy.get("button").click();
+    });
+    cy.route({
+      method: "PUT",
+      url: "**/tasks/1",
+      response: "fixture:task_confirmation_response.json",
+    });
+    cy.get("#confirm-task").click();
+    cy.get("#success-message").should(
+      "contain",
+      "Your task has been confirmed"
+    );
+  });
 
-  // it("Unsuccessfully - Bad address", () => {
-  //   cy.route({
-  //     method: "GET",
-  //     url: "https://maps.googleapis.com/maps/api/geocode/json?**",
-  //     response: "fixture:address_to_coords_bad_address_response.json",
-  //     params: {
-  //       address: "kjfhjgfjhgfjhf",
-  //       key: process.env.REACT_APP_GOOGLE_APIKEY,
-  //     },
-  //   });
-  //   cy.get("#create-request").click();
-  //   cy.get("#product-1").should("not.exist");
-  //   cy.get("#addressInput").type("kjfhjgfjhgfjhf");
-  //   cy.get("#addressConfirm").click();
+  it("Unsuccessfully - Bad address", () => {
+    cy.route({
+      method: "GET",
+      url: "https://maps.googleapis.com/maps/api/geocode/json?**",
+      response: "fixture:address_to_coords_bad_address_response.json",
+      params: {
+        address: "kjfhjgfjhgfjhf",
+        key: process.env.REACT_APP_GOOGLE_APIKEY,
+      },
+    });
+    cy.get("#create-request").click();
+    cy.get("#product-1").should("not.exist");
+    cy.get("#addressInput").type("kjfhjgfjhgfjhf");
+    cy.get("#addressConfirm").click();
 
-  //   cy.get("#success-message").should(
-  //     "contain",
-  //     "Your address could not be confirmed"
-  //   );
-  // });
+    cy.get("#success-message").should(
+      "contain",
+      "Your address could not be confirmed"
+    );
+  });
 
   it("Unsuccessfully - too few items", () => {
     cy.route({
