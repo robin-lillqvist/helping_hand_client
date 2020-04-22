2b5d5fe6dea43a86ade329d1b5d650c06bb5ce0a
Blake <blake@futchi.com>
2020-04-22 12:25:04 +0200

    setup grid structure

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import image from '../images/user-blank.jpg'
-import { Image } from 'semantic-ui-react'
+import { Image, Container, Grid } from 'semantic-ui-react'
 
  
 
 const ProfilePage = () => {
 
     return (
-      <Image src={image} size='small' />
+      <Container style={{marginTop: "15px"}}>
+        <Grid columns={2} divided>
+          <Grid.Column>
+            <Image src={image} size='small' />
+          </Grid.Column>
+          <Grid.Column>
+            <Container>
+              NAME:
+            </Container>
+            <Container style={{marginTop: "2px"}}>
+              ACTIVE REQUESTS
+            </Container>
+            <Container style={{marginTop: "2px"}}>
+              ACTIVE TASKS
+            </Container>
+          </Grid.Column>
+        </Grid>
+      </Container>
     )
   }
  
