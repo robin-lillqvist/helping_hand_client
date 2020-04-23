3e4376da95a0d934d5132ab6a51357ef18b05fd5
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-23 13:38:27 +0200

    [STATE] working state so far

--- src/components/DisplayMap.jsx
+++ src/components/DisplayMap.jsx
@@ -3,45 +3,41 @@ import { Marker, Map, InfoWindow, GoogleApiWrapper } from 'google-maps-react'
 import { connect, useDispatch } from 'react-redux'
 const MapContainer = props => {
   const dispatch = useDispatch()
-
   const style = { width: '100%', height: '100%' }
-  const [showPopUp, setShowPopUp] = useState(false)
-  const [activeRequest, setActiveMarker] = useState({})
-
-  const onMarkerClick = (props, request) => {
-    setActiveMarker(request)
+  // const [showPopUp, setShowPopUp] = useState(false)
+  const [activeMarker, setActiveMarker] = useState({})
+  const [selectedPlace, setSelectedPlace] = useState({})
+  const onMarkerClick = (props, marker, e) => {
+    setActiveMarker(marker)
+    setSelectedPlace(props)
+    // setShowPopUp(true)
     debugger
-    setShowPopUp(true)
   }
   return (
     <>
-      <div>
-        {showPopUp && (
-          <InfoWindow visible={showPopUp} marker={activeRequest}>
-            <div>
-              <p>Hello</p>
-            </div>
-          </InfoWindow>
-        )}
-        <Map
-          centerAroundCurrentLocation
-          key="google-map"
-          google={props.google}
-          style={style}
-          zoom={15}
-        >
-          {props.requests.map(request => (
-            <Marker
-              title={request.user.email}
-              name={request.user.email}
-              key={'marker-' + request.id}
-              id={'marker-' + request.id}
-              position={{ lat: request.lat, lng: request.long }}
-              onClick={() => onMarkerClick()}
-            />
-          ))}
-        </Map>
-      </div>
+      <Map
+        centerAroundCurrentLocation
+        key='google-map'
+        google={props.google}
+        style={style}
+        zoom={15}
+      >
+        {props.requests.map(request => (
+          <Marker
+            title={request.user.email}
+            name={request.user.email}
+            key={'marker-' + request.id}
+            id={'marker-' + request.id}
+            position={{ lat: request.lat, lng: request.long }}
+            onClick={onMarkerClick}
+          ></Marker>
+        ))}
+        <InfoWindow visible={true} marker={activeMarker}>
+          <div>
+            <p>{selectedPlace.title}</p>
+          </div>
+        </InfoWindow>
+      </Map>
     </>
   )
 }
@@ -51,7 +47,6 @@ const mapStateToProps = state => {
     showMarkerWindow: state.showMarkerWindow
   }
 }
-
 const Google = GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY
 })(MapContainer)
